1c49af28cfe0ad0ac695f586b80821d255a9022a
Jeremy Wright <jeremy@codestrokes.com>
2014-02-16 20:25:49 -0800

    removed compiled python files.

--- src/robotractor/backend/admin.py
+++ src/robotractor/backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from models import Farm, Tractor
+
+class FarmAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Farm, FarmAdmin)
+
+class TractorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Tractor, TractorAdmin)
 
 # Register your models here.
--- src/robotractor/backend/models.py
+++ src/robotractor/backend/models.py
@@ -1,5 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Farm(models.Model):
+    name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
+    
 class Tractor(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    operator = models.ForeignKey(User)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    altitude = models.FloatField()
+    farm = models.ForeignKey(Farm)
+    def __unicode__(self):
+        return "{0} @ {1},{2},{3} driven by {4}".format(self.name, self.latitude, self.longitude, self.altitude, self.operator)
+
+
+
+    
 	
