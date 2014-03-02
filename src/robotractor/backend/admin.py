bd0d50d24a3a03e3fd5ab93369faa774f3218a3a
Jeremy Wright <jeremy@codestrokes.com>
2014-03-02 13:15:46 -0800

    Added Farm models.

--- src/robotractor/backend/admin.py
+++ src/robotractor/backend/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Tractor, Farm, WorkingBoundary
+
+class TractorAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Tractor, TractorAdmin)
+
+class FarmAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Farm, FarmAdmin)
+
+class WorkingBoundaryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(WorkingBoundary, WorkingBoundaryAdmin)
+
 
 # Register your models here.
--- src/robotractor/backend/models.py
+++ src/robotractor/backend/models.py
@@ -1,5 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class WorkingBoundary(models.Model):
+    name = models.CharField(max_length=200)
+    nw_lat = models.FloatField()
+    nw_long = models.FloatField()
+    se_lat = models.FloatField()
+    se_long = models.FloatField()
+
+    def __unicode__(self):
+        return "{0} at {1}N {2}W by {3}N {4}W".format(self.name, self.nw_lat, self.nw_long, self.se_lat, self.se_long)
+
+class Farm(models.Model):
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.name
+
 
 class Tractor(models.Model):
-	name = models.CharField(max_length=100)
+    owner = models.ForeignKey(Farm)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    altitude = models.FloatField()
+    def __unicode__(self):
+        return self.name
+
 	
