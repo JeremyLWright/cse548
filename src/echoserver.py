9efffb2e3cd55925cacd0193e0b4bf0ee5c56147
Jeremy Wright <jeremy@codetrokes.com>
2014-03-29 18:07:45 +0000

    Echo server working.

---
+++ src/echoserver.py
@@ -0,0 +1,24 @@
+import logging
+
+from sleekxmpp import ClientXMPP
+from sleekxmpp.exceptions import IqError, IqTimeout
+
+class EchoBot(ClientXMPP):
+    def __init__(self, jid, password):
+        ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.session_start)
+        self.add_event_handler("message", self.message)
+        
+    def session_start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+if __name__ == '__main__':
+    xmpp = EchoBot('robot@54.83.55.95', 'robot')
+    xmpp.connect()
+    xmpp.process(block=True)
+	
