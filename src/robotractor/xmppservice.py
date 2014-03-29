1fdcafeccf393a57949e635bc84ca83b873ea19a
Jeremy Wright <jeremy@codetrokes.com>
2014-03-29 21:30:19 +0000

    Added xmpp connection to the database.

--- src/robotractor/xmppservice.py
+++ src/robotractor/xmppservice.py
@@ -3,10 +3,14 @@ import logging
 from sleekxmpp import ClientXMPP
 from sleekxmpp.exceptions import IqError, IqTimeout
 
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "robotractor.settings")
+from backend.models import Tractor
+from django.core import serializers
 
 class EchoBot(ClientXMPP):
     def __init__(self, jid, password):
-        ClientXMPP.__init__(self, jid, password)
+        super(EchoBot, self).__init__(jid, password)
         self.add_event_handler("session_start", self.session_start)
         self.add_event_handler("message", self.message)
         
@@ -16,9 +20,13 @@ class EchoBot(ClientXMPP):
 
     def message(self, msg):
         if msg['type'] in ('chat', 'normal'):
-            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+            #msg.reply("Processing command\n%(body)s" % msg)
+            #msg.reply("Now sending tou some databse stuff.")
+            data = serializers.serialize("json", Tractor.objects.all())
+            msg.reply(data).send()
 
 if __name__ == '__main__':
+    print Tractor.objects.all()
     xmpp = EchoBot('robot@54.83.55.95', 'robot')
     xmpp.connect()
     xmpp.process(block=True)
