9664dc42a1affbf3b850cc6950218a90c2174ae6
Jeremy Wright <jeremy@codetrokes.com>
2014-04-08 01:51:04 +0000

    Added test script to demonstrate integration of TPM.

--- src/robotractor/virtualTractor/tmp_test.py
+++ src/robotractor/virtualTractor/tmp_test.py
@@ -7,13 +7,22 @@ server = TPM("../keys/tractor01.pub", "../keys/robot-server.priv")
 
 
 #Generate a session key on the server to send to the tractor
-tractors_session_key = server.pki_encrypt(server.session_key)
+session_key = server.pki_encrypt(server.session_key)
 
-print "Session: "+tractors_session_key
+# Send David the session key
+
+#David initialized his TPM with the session key.
+my_key = tractor.pki_decrypt(session_key)
+tractor.session_key = my_key
+
+print "Session: "+tractor.session_key
 
 for i in range(10):
     t = tractor.encrypt("hello server")
     print "Ciphered: "+t
-    print "decrypted: "+ tractor.decrypt(t)
+    print "decrypted: "+ server.decrypt(t)
+
+    s = server.encrypt("Hello david.")
+    print tractor.decrypt(s)
 
 
